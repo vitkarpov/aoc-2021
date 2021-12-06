0928cc35a66cf39ec8a22281a906f63ef16e956c
vitkarpov <vitkor.s.karpov@gmail.com>
2021-12-06 14:22:55 +0000

    Day6

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,5 +20,5 @@ tasks {
 }
 
 application {
-    mainClassName = "Day05Kt"
+    mainClassName = "Day06Kt"
 }
---
+++ src/Day06.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+    fun part1(input: List<String>): Long {
+        check(input.size == 1)
+        return solve(input.first(), 80)
+    }
+
+    fun part2(input: List<String>): Long {
+        check(input.size == 1)
+        return solve(input.first(), 256)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 5934L)
+    check(part2(testInput) == 26984457539L)
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+}
+
+val dp = HashMap<Pair<Int, Int>, Long>()
+
+fun dfs(num: Int, days: Int): Long {
+    return when {
+        num == -1 -> dfs(6, days) + dfs(8, days)
+        days == 0 -> 1L
+        else -> dp[num to days] ?: dfs(num - 1, days - 1).also { dp[num to days] = it }
+    }
+}
+
+fun solve(input: String, days: Int) =
+    input.split(',').map { it.toInt() }.map { dfs(it, days) }.sum()
